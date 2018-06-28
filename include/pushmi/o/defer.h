ad907cc8581366d5d17da16e3b6977342adb313d
Kirk Shoop <kirkshoop@fb.com>
2018-06-27 17:58:58 -0700

    move operators and subject from examples to pushmi

--- include/pushmi/o/defer.h
+++ include/pushmi/o/defer.h
@@ -6,9 +6,9 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-#include <pushmi/single.h>
-#include <pushmi/o/submit.h>
-#include <pushmi/o/extension_operators.h>
+#include "../single.h"
+#include "submit.h"
+#include "extension_operators.h"
 
 #if __cpp_deduction_guides >= 201703
 #define MAKE(x) x MAKE_
