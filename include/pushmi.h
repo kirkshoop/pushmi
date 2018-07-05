aac1334f38202b37d72e96d818d387f070c80dd9
Kirk Shoop <kirkshoop@fb.com>
2018-07-05 10:21:39 -0700

    extract lambda from transform_fn

--- include/pushmi.h
+++ include/pushmi.h
@@ -5942,6 +5942,22 @@ namespace pushmi {
 
 namespace detail {
 
+
+// extracted this to workaround cuda compiler failure to compute the static_asserts in the nested lambda context
+template<class F>
+struct transform_on_value {
+  F f_;
+  template<class Out, class V>
+  auto operator()(Out& out, V&& v) {
+    using Result = decltype(f_((V&&) v));
+    static_assert(::pushmi::SemiMovable<Result>,
+      "none of the functions supplied to transform can convert this value");
+    static_assert(::pushmi::SingleReceiver<Out, Result>,
+      "Result of value transform cannot be delivered to Out");
+    ::pushmi::set_value(out, f_((V&&) v));
+  }
+};
+
 struct transform_fn {
   template <class... FN>
   auto operator()(FN... fn) const;
@@ -5950,6 +5966,7 @@ struct transform_fn {
 template <class... FN>
 auto transform_fn::operator()(FN... fn) const {
   auto f = ::pushmi::overload(std::move(fn)...);
+  using F = decltype(f);
   return ::pushmi::constrain(::pushmi::lazy::Sender<::pushmi::_1>, [f = std::move(f)](auto in) {
     using In = decltype(in);
     // copy 'f' to allow multiple calls to connect to multiple 'in'
@@ -5962,15 +5979,16 @@ auto transform_fn::operator()(FN... fn) const {
             std::move(out),
             // copy 'f' to allow multiple calls to submit
             ::pushmi::on_value(
-              [f](Out& out, auto&& v) {
-                using V = decltype(v);
-                using Result = decltype(f((V&&) v));
-                static_assert(::pushmi::SemiMovable<Result>,
-                  "none of the functions supplied to transform can convert this value");
-                static_assert(::pushmi::SingleReceiver<Out, Result>,
-                  "Result of value transform cannot be delivered to Out");
-                ::pushmi::set_value(out, f((V&&) v));
-              }
+              transform_on_value<F>{f}
+              // [f](Out& out, auto&& v) {
+              //   using V = decltype(v);
+              //   using Result = decltype(f((V&&) v));
+              //   static_assert(::pushmi::SemiMovable<Result>,
+              //     "none of the functions supplied to transform can convert this value");
+              //   static_assert(::pushmi::SingleReceiver<Out, Result>,
+              //     "Result of value transform cannot be delivered to Out");
+              //   ::pushmi::set_value(out, f((V&&) v));
+              // }
             )
           );
         })
