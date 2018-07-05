3c6a14717429925ef7c565f5a7eafc236169a7bc
Kirk Shoop <kirkshoop@fb.com>
2018-07-05 09:56:41 -0700

    workaround for cuda failure to resolve concepts in nested auto lambda

--- include/pushmi/o/transform.h
+++ include/pushmi/o/transform.h
@@ -21,25 +21,25 @@ struct transform_fn {
 
 template <class... FN>
 auto transform_fn::operator()(FN... fn) const {
-  auto f = overload(std::move(fn)...);
-  return constrain(lazy::Sender<_1>, [f = std::move(f)](auto in) {
+  auto f = ::pushmi::overload(std::move(fn)...);
+  return ::pushmi::constrain(::pushmi::lazy::Sender<::pushmi::_1>, [f = std::move(f)](auto in) {
     using In = decltype(in);
     // copy 'f' to allow multiple calls to connect to multiple 'in'
-    return ::pushmi::detail::deferred_from<In, single<>>(
+    return ::pushmi::detail::deferred_from<In, ::pushmi::single<>>(
       std::move(in),
       ::pushmi::detail::submit_transform_out<In>(
-        constrain(lazy::Receiver<_1>, [f](auto out) {
+        ::pushmi::constrain(::pushmi::lazy::Receiver<::pushmi::_1>, [f](auto out) {
           using Out = decltype(out);
           return ::pushmi::detail::out_from_fn<In>()(
             std::move(out),
             // copy 'f' to allow multiple calls to submit
-            on_value(
+            ::pushmi::on_value(
               [f](Out& out, auto&& v) {
                 using V = decltype(v);
                 using Result = decltype(f((V&&) v));
-                static_assert(SemiMovable<Result>,
+                static_assert(::pushmi::SemiMovable<Result>,
                   "none of the functions supplied to transform can convert this value");
-                static_assert(SingleReceiver<Out, Result>,
+                static_assert(::pushmi::SingleReceiver<Out, Result>,
                   "Result of value transform cannot be delivered to Out");
                 ::pushmi::set_value(out, f((V&&) v));
               }
